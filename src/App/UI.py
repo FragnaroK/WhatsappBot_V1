32e13f6aa46df4b4b533dedf9078cccb294c89f5
Franco <fcanalejo2000@outlook.com>
2021-12-28 22:09:15 +1000

    .

--- src/App/UI.py
+++ src/App/UI.py
@@ -1,20 +1,15 @@
-import eel
-import eel.browsers as browsers
-import sys
-import json
-from os import path
-from pathlib import Path
-from types import SimpleNamespace
 from json.encoder import JSONEncoder
-
-from App.models.userconfig import jsConfig, pyConfig
-
+from os import path
+import sys
+import eel
+import json
+from types import SimpleNamespace
 # determine if the application is a frozen `.exe` (e.g. pyinstaller --onefile) 
 if getattr(sys, 'frozen', False):
     application_path = path.dirname(sys.executable)
 # or a script file (e.g. `.py` / `.pyw`)
 elif __file__:
-    application_path = Path(path.dirname(__file__)).parent.absolute()
+    application_path = path.dirname(__file__)
     
 
 dir_path = application_path
@@ -24,11 +19,29 @@ class configEncoder(JSONEncoder):
     def default(self, o):
             return o.__dict__
         
+
+class schePhrases:
+    def __init__(self, phrase):
+        self.phrase = phrase.phrase
+        self.time = phrase.time
+# Parse JSON into an object with attributes corresponding to dict keys.
+
+class pyConfig:
+    def __init__(self, browser, phone):
+        self.browser = browser
+        self.phone = phone
+        
+class jsConfig:
+    def __init__(self, modes, rPhrases, sPhrases, interval, time):
+        self.modes = [modes.random, modes.scheduled]
+        self.randomPhrases = rPhrases
+        self.scheduledPhrases = sPhrases
+        self.every = interval
+        self.time = time
+        
 @eel.expose       
 def getpythonConfig(Config):
-    print("\nGeneral Data Saved:\n\n")
     print(Config)
-    print("\n")
     data = json.loads(str(Config), object_hook=lambda d: SimpleNamespace(**d))
     global botConfig
     botConfig = pyConfig(data.browser, data.phone)
@@ -36,12 +49,10 @@ def getpythonConfig(Config):
     
 @eel.expose       
 def getjsConfig(Config):
-    print("\nBot Data Saved:\n\n")
     print(Config)
-    print("\n")
     data = json.loads(str(Config), object_hook=lambda d: SimpleNamespace(**d))
     global userConfig
-    userConfig = jsConfig(data.modes, data.randomPhrases, data.scheduledPhrases, data.autoMessages, data.keyWord, data.every, data.times)
+    userConfig = jsConfig(data.modes, data.randomPhrases, data.scheduledPhrases , data.every, data.times)
     saveData(data, "userConfig")
 
 @eel.expose
@@ -51,18 +62,13 @@ def checkSavedData():
         with open(r"{}\config\userConfig.json".format(dir_path),
                 "r", encoding="utf8") as f1:
             setJsConfigFile = json.load(f1, object_hook=lambda d: SimpleNamespace(**d))
-            setJsConfig = jsConfig(setJsConfigFile.modes, setJsConfigFile.randomPhrases, setJsConfigFile.scheduledPhrases, setJsConfigFile.autoMessages, setJsConfigFile.keyWord , setJsConfigFile.every, setJsConfigFile.times)
+            setJsConfig = jsConfig(setJsConfigFile.modes, setJsConfigFile.randomPhrases, setJsConfigFile.scheduledPhrases, setJsConfigFile.every, setJsConfigFile.times) 
             f1.close()
         with open(r"{}\config\botConfig.json".format(dir_path),
                 "r", encoding="utf8") as f:
             pythonConfigFile = json.load(f, object_hook=lambda d: SimpleNamespace(**d))
             setPyConfig = pyConfig(pythonConfigFile.browser,pythonConfigFile.phone)
             f.close()
-        print("\nData to be loaded: \n\n")
-        print(pythonConfigFile)
-        print("\n")
-        print(setJsConfigFile)
-        print("\n\nData sent!\n")
         eel.getSavedData(json.dumps(setJsConfig, cls=configEncoder, ensure_ascii=False), json.dumps(setPyConfig, cls=configEncoder, ensure_ascii=False))
             
     
@@ -77,14 +83,12 @@ def startBot():
         import main
         print("Bot module Found!")
      except ModuleNotFoundError or ImportError as err:
-        print("[!] ERROR:  Bot Module Not Found")
+        print("[!] ERROR:  Selenium WebDriver Edge Not Found")
         print(err)
-     main.startFromUI()
+     main.runBot()
 
 def runUI():
-    print(dir_path)
-    eel.init('{}\\App\\web'.format(dir_path), allowed_extensions=['.js', '.html'])
-    browsers.set_path("chromium", r"{}\chrome-win\chrome.exe".format(dir_path))
-    eel.start('index.html', mode="chromium", cmdline_args=['--disable-extensions', '--test-type=gpu'])
+    eel.init('UI', allowed_extensions=['.js', '.html'])
+    eel.start('index.html')
 
     
